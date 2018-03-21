6330d818bba86ae28ea923a82b9ad0654a742982
Yanuar <yanuar.dev@gmail.com>
2018-03-21 19:21:53 +0700

    change a bit

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ app.get('/', (req, res) => {
     //         'Movies'
     //     ]
     // });
-
     res.render('home.hbs', {
         pageTitle: 'Home Page',
         welcomeMessage: 'Welcome to home page',
